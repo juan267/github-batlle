2f656a804e5c37e6d970216c48de95a2f18175dd
juan267 <j.gomez267@gmail.com>
2016-05-25 23:40:51 -0500

    Avatar paranet component

--- app/index.js
+++ app/index.js
@@ -1,12 +1,46 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var App = React.createClass({
-  render: function () {
+var USER_DATA = {
+  name: 'Juan Gomez',
+  username: 'juan267',
+  image: 'http://m.c.lnkd.licdn.com/mpr/mpr/AAEAAQAAAAAAAAMiAAAAJGVlYTU5Y2YyLWQwMzYtNDlmZS04MDdlLWI0ZjJjZWRhYjk4ZQ.jpg'
+}
+
+var Avatar = React.createClass({
+  render () {
     return (
-      <h1>hola Desde React</h1>
+      <div>
+        <Image src={this.props.user.image} />
+        <Name name={this.props.user.name} />
+        <Username username={this.props.user.username} />
+      </div>
     )
   }
 })
 
-ReactDOM.render(<App />, document.body)
+var Image = React.createClass({
+  render () {
+    return (
+      <img src= {this.props.src} style={{ width: 100, height: 100 }} />
+    )
+  }
+})
+
+var Username = React.createClass({
+  render () {
+    return (
+      <a href={'https://www.github.com/' + this.props.username}>{this.props.username}</a>
+    )
+  }
+})
+
+var Name = React.createClass({
+  render () {
+    return (
+      <p>{this.props.name}</p>
+    )
+  }
+})
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'))
